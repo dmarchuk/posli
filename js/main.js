c7d70d0a5b43b3dfe03ebb9f9b02a156465db372
Daniel Marchuk <dmarchuk@me.com>
2015-07-31 13:33:03 +0200

    Final changes

--- js/main.js
+++ js/main.js
@@ -11,22 +11,22 @@ $('button.get-in-line').click(function () {
 });
 
 function validateEmail($email) {
-  var email = $email.val();
-  var emailRegEx = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+    var email = $email.val();
+    var emailRegEx = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
 
-  if (emailRegEx.test(email)) {
-    $email.removeClass('error');
-  }
-  else {
-    $email.addClass('error').focus();
-    $('.error').off('keyup').on('keyup', function () {
-      if (validateEmail($(this))) {
-        $(this).removeClass('error');
-        $('.error').off('keyup');
-      }
-    });
-  }
-  return emailRegEx.test(email);
+    if (emailRegEx.test(email)) {
+        $email.removeClass('error');
+    }
+    else {
+        $email.addClass('error').focus();
+        $('.error').off('keyup').on('keyup', function () {
+            if (validateEmail($(this))) {
+                $(this).removeClass('error');
+                $('.error').off('keyup');
+            }
+        });
+    }
+    return emailRegEx.test(email);
 }
 
 function successFunction() {
@@ -39,19 +39,40 @@ function successFunction() {
 }
 
 $('button.submit').click(function () {
-  var $email = $(this).parent().find('input');
-  if (validateEmail($email)) {
-    successFunction();
-  }
+    var $email = $(this).parent().find('input');
+    if (validateEmail($email)) {
+        successFunction();
+    }
 });
-$(document).on("scroll", onScroll);
+
+$(window).scroll(function() {
+
+    var menuScroll = $('#scroll-menu')
+
+    if ($(window).width() <= 767) {
+
+        if ($(window).scrollTop() > 524) {
+            menuScroll.addClass('mobile-fixed');
+        } else {
+            menuScroll.removeClass('mobile-fixed');
+        }
+
+    }
+
+    if ($(window).scrollTop() > 300) {
+        menuScroll.show();
+    } else {
+        menuScroll.hide();
+    }
+
+})
 
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
     $('#scroll-menu a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        if (refElement.position().top - $('#scroll-menu').height()  <= scrollPos && refElement.position().top - $('#scroll-menu').height() + refElement.height() > scrollPos) {
             $('#scroll-menu a').removeClass("active");
             currLink.addClass("active");
         }
@@ -59,11 +80,14 @@ function onScroll(event){
             currLink.removeClass("active");
         }
     });
+}
 
+function onScrollMobile(event){
+    var scrollPos = $(document).scrollTop();
     $('.menu a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        if (refElement.position().top - $('#scroll-menu').height()  <= scrollPos && refElement.position().top - $('#scroll-menu').height() + refElement.height() > scrollPos) {
             $('.menu a').removeClass("active");
             currLink.addClass("active");
         }
@@ -73,23 +97,13 @@ function onScroll(event){
     });
 }
 
-$(window).scroll(function() {
-
-    var menuEl = $('.menu');
-
-    if ($(window).width() <= 767) {
-
-        if ($(window).scrollTop() > 524) {
-            menuEl.addClass('mobile-fixed');
-        } else {
-            menuEl.removeClass('mobile-fixed');
-        }
-
-    }
-
-})
-
 $(document).ready(function() {
+
+    if ($(window).width <= 767) {
+        $(document).on("scroll", onScrollMobile);
+    } else {
+        $(document).on("scroll", onScroll);
+    }
 
     var w = 0;
 
